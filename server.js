c7f5ba36a34c08d16becfadfd707a3766ddd3d11
sumrender <sumrenders@gmail.com>
2023-08-05 14:41:15 +0530

    new assets

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ const publicFolderPath = path.join(__dirname, "public");
 
 app.use(express.static(publicFolderPath));
 
+app.get("/", (req, res) => {
+  res.send(`server is live.`);
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server started on port ${port}`);
